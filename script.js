fb7b01779301519337ca8d7dfb353201486cbab1
Anton Tarasov <toxa-tarasov@mail.ru>
2021-10-01 20:14:15 +0300

    поработал со скриптом

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("Привет, куратор!")
+console.log("Привет, куратор!")
+let title = 'title';
+let screens = 'screens';
+let screenPrice = 'screenPrice';
+let rollback = 'rollback';
+let fullPrice = 'fullPrice';
+let adaptive = 'adaptive';
