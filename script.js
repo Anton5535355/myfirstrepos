81b085a6f46b296c23aea604b89298ffb5bcd12c
Anton Tarasov <toxa-tarasov@mail.ru>
2021-10-02 17:50:17 +0300

    сделал домашнее задание lesson02

--- script.js
+++ script.js
@@ -7,11 +7,20 @@ rollback,
 fullPrice, 
 adaptive;
 
-title = project;
+title = document;
 screens = 'Простые, сложные, интерактивные';
 screenPrice = 290;
 rollback = 99;
-fullPrice = 100000000000n;
+fullPrice = 100000000000;
 adaptive = true;
 
-console.log(project, fullPrice, adaptive)
+console.log(typeof document);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов 290 рублей')
+console.log('Стоимость разработки сайта 100000000000n рублей');
+console.log(screens.toLowerCase())
+console.log(screens.split(" "))
+console.log(100000000000 * (90/100));
+
