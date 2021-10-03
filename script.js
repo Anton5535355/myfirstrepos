42dcf2585f5fe849200d1710ebe96118ec4d091d
Anton Tarasov <toxa-tarasov@mail.ru>
2021-10-03 21:04:16 +0300

    исправил ошибки в дз 2

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
 alert("Привет, куратор!");
 console.log("Привет, куратор!");
-let title, 
-screens, 
-screenPrice, 
-rollback, 
-fullPrice, 
-adaptive;
 
-title = document;
-screens = 'Простые, сложные, интерактивные';
-screenPrice = 290;
-rollback = 99;
-fullPrice = 100000000000;
-adaptive = true;
 
-console.log(typeof document);
+let title = 'Мое второе дз';
+let screens = 'Простые, сложные, интерактивные';
+let screenPrice = 290;
+let rollback = 99;
+let fullPrice = 100000000000;
+let adaptive = true;
+
+console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
-console.log('Стоимость верстки экранов 290 рублей')
-console.log('Стоимость разработки сайта 100000000000n рублей');
-console.log(screens.toLowerCase())
-console.log(screens.split(" "))
+console.log('Стоимость верстки экранов' + screenPrice + 'рублей');
+console.log('Стоимость разработки сайта' + fullPrice + 'рублей');
+console.log(screens.toLowerCase());
+console.log(screens.split(" "));
 console.log(100000000000 * (90/100));
-
