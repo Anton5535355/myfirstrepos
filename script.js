c6163452a5a8bd89d53d4fc3cf335de643b23a6a
Anton Tarasov <toxa-tarasov@mail.ru>
2021-10-04 12:12:32 +0300

    исправление

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log("Привет, куратор!");
 
 
 let title = 'Мое второе дз';
-let screens = 'Простые, сложные, интерактивные';
+let screens = 'ПРОСТЫЕ, СЛОЖНЫЕ, ИНТЕРАКТИВНЫЕ';
 let screenPrice = 290;
 let rollback = 99;
 let fullPrice = 100000000000;
@@ -13,8 +13,8 @@ console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 console.log(screens.length);
-console.log('Стоимость верстки экранов' + screenPrice + 'рублей');
-console.log('Стоимость разработки сайта' + fullPrice + 'рублей');
+console.log('Стоимость верстки экранов' + " " + screenPrice + " " + 'рублей');
+console.log('Стоимость разработки сайта' + " "  + fullPrice + " " + 'рублей');
 console.log(screens.toLowerCase());
 console.log(screens.split(" "));
-console.log(100000000000 * (90/100));
+console.log(fullPrice * (rollback/100));
