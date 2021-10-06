fef9e157046631b2e448bb72f36afd4316713f88
Anton Tarasov <toxa-tarasov@mail.ru>
2021-10-06 19:40:48 +0300

    сделал 3 урок

--- script.js
+++ script.js
@@ -1,68 +1,44 @@
 'use strict';
-alert("Привет, куратор!");
-console.log("Привет, куратор!");
 
-
-let title = 'project';
-let screens = 'ПРОСТЫЕ, СЛОЖНЫЕ, ИНТЕРАКТИВНЫЕ';
+let title = prompt('Как называется ваш проект?', "project");
+let screens = prompt('Какие типы экранов нужно разработать?', "ПРОСТЫЕ, СЛОЖНЫЕ, ИНТЕРАКТИВНЫЕ");
 let screenPrice = 290;
 let rollback = 99;
 let fullPrice = 10000;
 let adaptive = true;
-
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log('Стоимость верстки экранов' + " " + screenPrice + " " + 'рублей');
-console.log('Стоимость разработки сайта' + " "  + fullPrice + " " + 'рублей');
-console.log(screens.toLowerCase());
-console.log(screens.toLowerCase().split(" "));
-console.log(fullPrice * (rollback/100));
-
-title = prompt('Как называется ваш проект?', "project");
-screens = prompt('Какие типы экранов нужно разработать?', "ПРОСТЫЕ, СЛОЖНЫЕ, ИНТЕРАКТИВНЫЕ");
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько это будет стоить?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько это будет стоить?');
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+let allServicePrices = getAllServicePrices()
 screenPrice = +prompt('Сколько будет стоить данная работа?', "290");
 adaptive = confirm('Нужен ли адаптив на сайте?');
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-    console.log(service1);
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-    console.log(servicePrice1);
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-    console.log(service2);
-let servicePrice2 = +prompt('Сколько это будет стоить?');
-    console.log(servicePrice2);
 fullPrice = (screenPrice + servicePrice1 + servicePrice2);
-    console.log(fullPrice);
-
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback/100)));
-    console.log(servicePercentPrice);
-
-if (fullPrice >= 30000) {
-    console.log('Даем скидку в 10%')
-}
-if (15000 <= fullPrice && fullPrice < 30000) {
-    console.log('Даем скидку в 5%')
-}
-if (15000 > fullPrice && fullPrice >= 0) {
-    console.log('Скидка не предусмотрена')
-}
-if (fullPrice < 0) {
-    console.log('Что то пошло не так')
-}
-
-
-let allServicePrices = servicePrice1 + servicePrice2;
-
-const getAllServicePrices = function(allServicePrices) {
-    return allServicePrices
-}
-
-getAllServicePrices()
-allServicePrices = getAllServicePrices(allServicePrices);
-
 fullPrice = screenPrice + allServicePrices;
+servicePercentPrice = getServicePercentPrices();
+
+
+const getRollbackMessage = function(price) {
+    if (price >= 30000) {
+        return 'Даем скидку в 10%'
+    }
+    else if (15000 <= price && price < 30000) {
+        return 'Даем скидку в 5%'
+    }
+    else if (15000 > price && price >= 0) {
+        return 'Скидка не предусмотрена'
+    }
+    else if (price < 0) {
+        return 'Что то пошло не так'
+    }
+}
+
+
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;
+}
+
 
 function getFullPrice(fullPrice) {
     function fullPrice(scP, aSP) {
@@ -70,12 +46,50 @@ function getFullPrice(fullPrice) {
     }
     fullPrice(screenPrice, allServicePrices);
 }
-getFullPrice()
 
-const getTitle = function(title) {
-    return title.ucFirst("project") == "Project"
+
+const getTitle = function() {
+    if (!title) return title;
+
+    return title[0].toUpperCase() + title.slice(1);
 }
+
+
+const getServicePercentPrices = function() {
+    return fullPrice - (fullPrice * (rollback/100))
+}
+
+
+const showTypeOf = function(a) {
+    console.log(a, typeof a)
+}
+
+
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+getAllServicePrices();
+getFullPrice();
 getTitle()
+getServicePercentPrices()
 
-console.log(allServicePrices);
 
+
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log(fullPrice);
+console.log(servicePercentPrice);
+console.log(getRollbackMessage(fullPrice));
+console.log(screens.length);
+console.log('Стоимость верстки экранов' + " " + screenPrice + " " + 'рублей');
+console.log('Стоимость разработки сайта' + " "  + fullPrice + " " + 'рублей');
+console.log(screens.toLowerCase());
+console.log(screens.toLowerCase().split(" "));
+console.log(fullPrice * (rollback/100));
+console.log(getAllServicePrices());
+console.log(getTitle());
+console.log(getServicePercentPrices());
+console.log(screens.split(" "));
